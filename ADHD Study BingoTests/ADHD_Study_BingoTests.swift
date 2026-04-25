edc52db71edbb1b64c5b7731e69f0d09e98bb88e
Mirro <mirro@zinuo.cc>
2026-04-25 18:16:10 -0400

    Initial Commit

---
+++ ADHD Study BingoTests/ADHD_Study_BingoTests.swift
@@ -0,0 +1,16 @@
+//
+//  ADHD_Study_BingoTests.swift
+//  ADHD Study BingoTests
+//
+//  Created by Mirro on 4/25/26.
+//
+
+import Testing
+
+struct ADHD_Study_BingoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
